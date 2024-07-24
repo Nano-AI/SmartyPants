f4533ab20d15f45cc06782331b8957de54cbd5a2
BerkM125 <49427416+BerkM125@users.noreply.github.com>
2024-07-23 21:38:07 -0700

    adjusting config for deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,9 @@ import svgLoader from "vite-svg-loader"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5000,
+  }
 })
